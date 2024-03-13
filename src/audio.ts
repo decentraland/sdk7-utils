e656cc06fda59044a2b1b07676a572b38f205cab
Nicolas Echezarreta <nicoecheza@gmail.com>
2024-03-13 16:52:34 -0300

    remove get audio duration method

--- src/audio.ts
+++ src/audio.ts
@@ -1,33 +1,9 @@
 import { AudioSource, Entity, EntityState, IEngine, PBAudioSource, engine } from "@dcl/sdk/ecs";
 import { priority } from "./priority";
 
-function getAudioDuration(audio: HTMLAudioElement): Promise<number> {
-  return new Promise((resolve, reject) => {
-    let loaded: boolean = false
-    const error = new Error('Failed getting sound duration')
+export type Sounds = ReturnType<typeof createSounds>
 
-    const timeout = setTimeout(() => {
-      if (!loaded) reject(error)
-      audio.removeEventListener("error", onError)
-      audio.removeEventListener("loadedmetadata", onLoadedMetadata)
-    }, 100)
-
-    function onError() {
-      clearTimeout(timeout)
-      reject(error)
-    }
-
-    function onLoadedMetadata() {
-      loaded = true
-      resolve(audio.duration)
-    }
-
-    audio.addEventListener("loadedmetadata", onLoadedMetadata)
-    audio.addEventListener("error", onError)
-  })
-}
-
-function assertSound(start: number, end: number, duration: number) {
+function assertSound(start: number, end: number) {
   if (start < 0) {
     throw new Error('Invalid "start" parameter provided. "start" parameter should be >= 0.')
   }
@@ -35,14 +11,10 @@ function assertSound(start: number, end: number, duration: number) {
   if (start >= end) {
     throw new Error('Invalid "start" & "end" parameters provided. "start" parameter should be lower than "end" parameter.')
   }
-
-  if (end > duration) {
-    throw new Error('Invalid "end" & "duration" parameters provided. "end" parameter should be lower than the duration of the sound.')
-  }
 }
 
 function createSounds(targetEngine: IEngine) {
-  const soundMap = new Map<Entity, { currentTime: number; end: number; duration: number; }>()
+  const soundMap = new Map<Entity, { currentTime: number; end: number; }>()
 
   function makeSystem(dt: number) {
     const deadSounds = [];
@@ -70,19 +42,14 @@ function createSounds(targetEngine: IEngine) {
   }
 
   function playSoundSegment(entity: Entity, value: PBAudioSource, start: number, end: number) {
-    const audio = new Audio(value.audioClipUrl)
-    getAudioDuration(audio)
-      .then(duration => {
-        assertSound(start, end, duration)
+    assertSound(start, end)
 
-        soundMap.set(entity, { currentTime: start, end, duration })
-        AudioSource.createOrReplace(entity, {
-          ...value,
-          playing: true,
-          currentTime: start
-        })
-      })
-      .catch((e) => console.error(e))
+    soundMap.set(entity, { currentTime: start, end })
+    AudioSource.createOrReplace(entity, {
+      ...value,
+      playing: true,
+      currentTime: start
+    })
   }
 
   targetEngine.addSystem(makeSystem, priority.TweenSystemPriority)
